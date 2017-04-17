ce87b6ae60bc61aa62534b60208165b09d30c888
Christian Zufferey <christian@zufferey.com>
2017-04-17 23:44:51 +0200

    Update install.sh
    Il manquait:
    sudo apt-get install -y gnupg2

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #Installation de l'application
-#zf170417.2300
+#zf170417.2344
 
 echo "ATTENTION, il ne faut pas configurer Mutt quand on nous le demande !" ; read
 
 sudo apt-get update
 sudo apt-get install -y mutt
+sudo apt-get install -y gnupg2
 
 ./acb_uncrypt.sh
 
