48c387b933d35b4a7225ad5d037e4260cf7a8309
Christian Zufferey <christian@zufferey.com>
2017-04-18 17:26:17 +0200

    - commencé à faire la boucle for pour l'envoi en groupe en fonction d'une liste en CSV

--- send_emails.sh
+++ send_emails.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
-#Démarrage de l'application
-#zf170417.1458
+#!/usr/bin/env bash
+#send_emails, Publipostage groupé par email
+#zf170418.1657
 
-bla bla bla
+echo ---------- send_emails start
+echo "Certain que vous vouliez envoyer le publipostage ? (CTRL+C pour arrêter): " ; read
+
+
+
+
+while IFS=, read col1 col2
+do
+    echo "Envoi de $col2 à $col1"
+
+  mutt -s "Fablab, cotisations 2017" $col1 < ./corps_email.txt -a ./annexes/$col2
+  sleep 3
+
+done < liste_publipost.csv
--- test_mutt.sh
+++ test_mutt.sh
@@ -2,4 +2,4 @@
 #Démarrage de l'application
 #zf170417.1458
 
-mutt -s "Test from mutt" christian.zufferey@epfl.ch < ./test.txt -a ./test.pdf
+mutt -s "Test d'envoi des cotisations 2017" christian.zufferey@epfl.ch < ./corps_email.txt -a ./test.pdf
