41be5eb9a685800c0ee49ca1884feaa623200a09
Christian Zufferey <christian@zufferey.com>
2020-12-22 00:46:47 +0100

    Corrigé le bug du CR à la fin du fichier .csv - je ne sais pas pourquoi, mais maintenant mes fichiers CSV venant de MAC ont un CRLF comme new line, il faut donc enlever le CR pour que ma moulinette fonctionne

--- send_emails.sh
+++ send_emails.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #send_emails, Publipostage groupé par email
-#zf181223
+#zf181223, zf201222.0046
 
 echo ---------- send_emails start
 echo "Certain que vous vouliez envoyer le publipostage ? (CTRL+C pour arrêter): " ; read
 
 
-
+sed -i 's/\r$//g' liste_publipost.csv
 
 while IFS=, read col1 col2 col3 col4 col5
 do
